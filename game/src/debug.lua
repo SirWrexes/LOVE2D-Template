c1bd4de148a5b20852388ecfbf0456e59fafc6b6
Ludovic Fernandez <ludofernandez@msn.com>
2025-04-07 22:33:37 +0200

    How did I gaslight myself into thinking `break` doesn't exist in Lua??

--- game/src/debug.lua
+++ game/src/debug.lua
@@ -4,10 +4,9 @@ if os.getenv "LOCAL_LUA_DEBUGGER_VSCODE" == "1" then
     for _, v in ipairs(arg) do
         if v == "-debug" then
             DEBUG = true
-            goto DEBUG_SET -- I wish `break` keyword existed in Lua...
+            break
         end
     end
-    ::DEBUG_SET::
 
     if DEBUG then
         local lldebugger = require "lldebugger"
